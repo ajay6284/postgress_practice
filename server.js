40d8d14ebff1af671cf56aaa4c223ef57bd9f0f9
ajay6284 <mrajay373@gmail.com>
2025-06-07 16:22:27 +0530

    implemented the transaction using 'BEGIN' and 'COMMIT'

--- server.js
+++ server.js
@@ -15,24 +15,35 @@ app.post('/signup', async (req, res) => {
     const { country } = req.body;
     const { street } = req.body;
     const { pincode }  = req.body;
+   
+     try{
+        
+        const insertOuery = `INSERT INTO users(username, email, password) VALUES ($1, $2, $3) RETURNING id;`;
+        const addressQuery =`INSERT INTO addresses(user_id, city, country, street, pincode)
+        VALUES ($1, $2, $3, $4, $5);`;
 
-    const insertOuery = `INSERT INTO users(username, email, password) VALUES ($1, $2, $3) RETURNING id;`;
-    const response = await pgClient.query(insertOuery, [username, email, password]);
-
-
-    const addressQuery =
-     `INSERT INTO addresses(user_id, city, country, street, pincode)
-     VALUES ($1, $2, $3, $4, $5);`;
-
-     const user_id = response.rows[0].id;
-
-     const response2 = await pgClient.query(addressQuery,[user_id, city, country, street, pincode]);
-
-    if(response2){
-        console.log(response2.rows);
+        await pgClient.query('BEGIN');
+        const response = await pgClient.query(insertOuery, [username, email, password]);
+        await new Promise(x => setTimeout(x, 200 * 1000));
+        const user_id = response.rows[0].id;
+        const response2 = await pgClient.query(addressQuery,[user_id, city, country, street, pincode]);
+        await pgClient.query('COMMIT');
+        
+            if(response2){
+                console.log(response2.rows);
+                res.status(200).json({
+                    message: 'user sucessfully signup',            
+                })
+            }
     }
-});
+    catch(e){
+        res.status(404).json({
+            message: 'Error in user creation',
+            error: e.message
+        })
+    }
+    });
 
-app.listen(3000);
+    app.listen(3000);
 
 
